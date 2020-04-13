b90e1914e836950b0c38c646a1d2641e0e9f0a8e
franathan1 <franeyjohn96@gmail.com>
2020-04-13 13:04:41 -0500

    finalized and cleaned code for usgs_datagrab.py

--- usgs_datagrab.py
+++ usgs_datagrab.py
@@ -1,42 +1,43 @@
 def usgs_datagrab():
+    """
+    usgs_datagrab accesses the USGS NWIS website and scraps the water surface level above NAVD 88 for available Lakes
+
+    :return: pandas multindex dataframe of lake name, USGS site ID, date of data acquisition, and associated water level
+    """
+    from datetime import datetime
     import urllib3
     import certifi
     import re
     import pandas as pd
 
-    target_url = 'https://nwis.waterdata.usgs.gov/usa/nwis/uv/?referred_module=sw&site_tp_cd=LK&index_pmcode_72020=' \
-                 '1&index_pmcode_72150=1&index_pmcode_72292=1&index_pmcode_62615=1&index_pmcode_62617=1&group_key=' \
-                 'NONE&format=sitefile_output&sitefile_output_format=rdb&column_name=site_no&range_selection=' \
-                 'date_range&begin_date=1950-10-01&end_date=2020-03-31&date_format=YYYY-MM-DD&rdb_compression=' \
-                 'file&list_of_search_criteria=site_tp_cd%2Crealtime_parameter_selection'
+    begin_date = '1776-7-4'
+    now = datetime.now()
+    end_date = now.strftime('%Y-%m-%d')
+
+    target_url = 'https://waterdata.usgs.gov/nwis/dv?referred_module=sw&site_tp_cd=LK&index_pmcode_62615=1&group_key=' \
+                 'NONE&sitefile_output_format=rdb&column_name=site_no&column_name=station_nm&range_selection=' \
+                 'date_range&begin_date={}&end_date={}&format=rdb&date_format=YYYY-MM-DD&rdb_compression=value' \
+                 '&list_of_search_criteria=site_tp_cd%2Crealtime_parameter_selection'.format(begin_date, end_date)
 
     http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
     req = http.request('GET', target_url)
     text = req.data.decode()
+
     retrieved_date = re.search(r"retrieved:.*$", text, re.M)
-    print(retrieved_date.group())
-    site_id_list = []
-    for line in text.split('\n'):
-        site_id = re.match(r"\d*[^A-Za-z]\b", line)
-        if site_id:
-            site_id_list.append(str(site_id.group()))
-    print(site_id_list)
+    print('This data was accessed on: {}'.format(retrieved_date.group()))
 
-    for ids in site_id_list:
-        usgs_url = 'https://nwis.waterdata.usgs.gov/me/nwis/dv?cb_62615=on&format=rdb&site_no={}' \
-                   '&referred_module=sw&period=&begin_date=1838-01-01&end_date=2020-04-09'.format(ids)
+    site_ids = re.findall(r"USGS (\d*) (.*)$", text, re.M)
+    temp_column_names = ['site_id', 'site_name']
+    name_df = pd.DataFrame(site_ids, columns=temp_column_names)
 
-        http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
-        req = http.request('GET', usgs_url)
-        text = req.data.decode()
-        time_series = []
-        for line in text.split('\n'):
-            comment_line = re.match(r"^[^#\n]", line)
-            if comment_line:
-                time_series.append(line)
-        df = pd.DataFrame([item.split('\t') for item in time_series])
-    return df
+    data = re.findall(r"(USGS)\t(\d*)\t([\d-]*)\t([\d.]*)", text)
 
+    column_names =['Agency', 'site_id', 'date', 'water_level_ft']
+    temp_usgs_dataframe = pd.DataFrame(data, columns=column_names)
 
-usgs_datagrab()
+    usgs_dataframe = temp_usgs_dataframe.merge(name_df)
+    usgs_dataframe.set_index(['site_id', 'site_name', 'date'], inplace=True)
+    print(usgs_dataframe)
+
+    return usgs_dataframe
 
