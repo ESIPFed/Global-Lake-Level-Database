82f124d21e6a5e11b4776b007341c284d361b271
jakegearon <uFZ!_g68msGpyWXoEzot>
2020-07-29 16:22:16 -0500

    rewriting USGS to intake waterML

--- Standardize.py
+++ Standardize.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import re
+from difflib import get_close_matches
 from usgs_datagrab import usgs_datagrab
 from grealm_data_multindex import grealm_data_multindex
 from HydroWeb_grabber import hydro_grab
@@ -28,10 +30,39 @@ df_usgs_f['water_level_ft'] = pd.to_numeric(df_usgs_f['water_level_ft'])
 df_usgs_f['water_level'] = df_usgs_f['water_level_ft'] * .3048
 df_usgs_f = df_usgs_f.drop(['water_level_ft'], axis=1)
 
-grlm_names = [i.lower().strip('lake ') for i in (df_grealm_f.index.levels[0])]
-usgs_names = [i.lower().strip('lake ') for i in (df_hydroweb_f.index.levels[0])]
-hydro
-
-
+# grlm_names = [i.lower() for i in df_grealm_f.index.levels[0]]
+# hydro_names = [i.lower().split('\blake\b')[1].strip(' ') for i in (df_hydroweb_f.index.levels[0])]
+# usgs_names = [re.split('\sat|\snear\s|\sabove\s|\seast\sof\s|\snorth\sof\s|\ssouth\sof\s|\swest\sof\s|\sn\sof\s|\ss\sof\s|\sw\sof\s|\se\sof\s|\snr\s|\sabv\s|\sab\s|\son\s|\s@\s',
+#                        i.lower())[0] for i in df_usgs_f.index.levels[0]]
+#
+# replacements = {'lake':'',
+#                 'lk': '',
+#                 'res': '',
+#                 'reservoir':'',
+#                 'pond':'',
+#                 'lago':''}
+# def replace(match):
+#     return replacements[match.group(0)]
+#
+# stripped_usgs = []
+# for i in usgs_names:
+#     strp = re.sub('|'.join(r'\b%s\b' % re.escape(s) for s in replacements), replace, i)
+#     stripped_usgs.append(strp.strip())
+#
+# #stripped_usgs = [i.replace(' lake\b', '').replace(' lk\b', '').replace(' res\b', '').replace(' reservoir\b','').replace(' pond ', '').replace(' lago ', '') for i in usgs_names]
+#
+# # srch = re.compile(r'(.*) (at|near|above|east of|north of|south of|west of|n of|s of|w of|e of|nr|abv)')
+# # .split('near')[1].split('above')[1].split('east of')[1].split('north of').split('south of').split('west of').split('n of').split('s of').split('w of').split('e of').split('nr').split('abv')
+# # result = [f.group(1) for f in (srch.match(line) for line in usgs_names) if f]
+# # result2 = [f.group(1) for f in (srch.match(line) for line in result) if f]
+# cnt = 0
+# for i in df_usgs_f.index.levels[0]:
+#     #match = get_close_matches(i, hydro_names)
+#     matching = [s for s in hydro_names if i == s]
+#     m = re.split('\sat|\snear\s|\sabove\s|\seast\sof\s|\snorth\sof\s|\ssouth\sof\s|\swest\sof\s|\sn\sof\s|\ss\sof\s|\sw\sof\s|\se\sof\s|\snr\s|\sabv\s', i.lower())
+#     #df_usgs_f.index.levels[0]]
+#     print(i, m)
+#     cnt += 1
+# print(cnt)
 
 
--- usgs_datagrab.py
+++ usgs_datagrab.py
@@ -1,48 +1,151 @@
-def usgs_datagrab():
-    """
-    usgs_datagrab accesses the USGS NWIS website and scraps the water surface level above NAVD 88 for available Lakes
+import io
 
-    :return: pandas multindex dataframe of lake name, USGS site ID, date of data acquisition, and associated water level
-    """
-    from datetime import datetime
-    import urllib3
-    import certifi
-    import re
-    import pandas as pd
+# def usgs_datagrab():
+import lxml
+import xml.etree.cElementTree as cET
 
-    begin_date = '1776-7-4'
-    now = datetime.now()
-    end_date = now.strftime('%Y-%m-%d')
+"""
+usgs_datagrab accesses the USGS NWIS website and scraps the water surface level above NAVD 88 for available Lakes
 
-    target_url = 'https://waterdata.usgs.gov/nwis/dv?referred_module=sw&site_tp_cd=LK&index_pmcode_62615=1&group_key=' \
-                 'NONE&sitefile_output_format=rdb&column_name=site_no&column_name=station_nm&range_selection=' \
-                 'date_range&begin_date={}&end_date={}&format=rdb&date_format=YYYY-MM-DD&rdb_compression=value' \
-                 '&list_of_search_criteria=site_tp_cd%2Crealtime_parameter_selection'.format(begin_date, end_date)
-    target_url = 'https://waterdata.usgs.gov/nwis/dv?referred_module=sw&site_tp_cd=LK&group_key=NONE&sitefile_output_format=rdb&column_name=station_nm&column_name=dec_lat_va&column_name=dec_long_va&range_selection=date_range&begin_date=1838-01-01&end_date=2020-06-12&format=rdb&date_format=YYYY-MM-DD&rdb_compression=value&list_of_search_criteria=site_tp_cd%2Crealtime_parameter_selection'
-    target_url = 'https://waterdata.usgs.gov/nwis/dv?referred_module=sw&site_tp_cd=LK&index_pmcode_62615=1&index_pmcode_62614=1&group_key=NONE&sitefile_output_format=rdb&column_name=station_nm&column_name=dec_lat_va&column_name=dec_long_va&column_name=land_net_ds&column_name=map_nm&range_selection=date_range&begin_date=1838-01-01&end_date=2020-06-12&format=rdb&date_format=YYYY-MM-DD&rdb_compression=value&list_of_search_criteria=site_tp_cd%2Crealtime_parameter_selection'
-    http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
-    req = http.request('GET', target_url)
-    text = req.data.decode()
+:return: pandas multindex dataframe of lake name, USGS site ID, date of data acquisition, and associated water level
+"""
+from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
-    retrieved_date = re.search(r"retrieved:.*$", text, re.M)
-    print('This data was accessed on: {}'.format(retrieved_date.group()))
+begin_date = '1838-01-01'
+now = datetime.now()
+end_date = now.strftime('%Y-%m-%d')
+# TODO Document what each parameter means or have a link to USGS website explaining
+param_list = ['72020','99020','30211','00062','99065',
+              '30207','00065','62600','72292','62615','62614','62616',
+              '62617','72214','72264','72275','72333','72335','72336',
+              '72199','72178']
 
-    site_ids = re.findall(r"USGS (\d*) (.*)$", text, re.M)
-    temp_column_names = ['site_id', 'site_name']
-    name_df = pd.DataFrame(site_ids, columns=temp_column_names)
+lake_list = 'https://waterdata.usgs.gov/nwis/dv?referred_module=sw&' \
+            'site_tp_cd=LK&index_pmcode_72020=1&' \
+            'index_pmcode_99020=1&' \
+            'index_pmcode_30211=1&' \
+            'index_pmcode_00062=1&' \
+            'index_pmcode_99065=1&' \
+            'index_pmcode_30207=1&' \
+            'index_pmcode_00065=1&' \
+            'index_pmcode_62600=1&' \
+            'index_pmcode_72292=1&' \
+            'index_pmcode_72293=1&' \
+            'index_pmcode_62615=1&' \
+            'index_pmcode_62614=1&' \
+            'index_pmcode_62616=1&' \
+            'index_pmcode_62617=1&' \
+            'index_pmcode_72214=1&' \
+            'index_pmcode_72264=1&' \
+            'index_pmcode_72275=1&' \
+            'index_pmcode_72333=1&' \
+            'index_pmcode_72335=1&' \
+            'index_pmcode_72336=1&' \
+            'index_pmcode_72199=1&' \
+            'index_pmcode_72178=1&' \
+            'group_key=NONE&format=sitefile_output&sitefile_output_format=xml&' \
+            'column_name=site_no&column_name=station_nm&range_selection=date_range&' \
+            'begin_date=1838-01-01&end_date=2020-06-16&date_format=YYYY-MM-DD&rdb_compression=file&' \
+            'list_of_search_criteria=site_tp_cd%2Crealtime_parameter_selection'
 
-    data = re.findall(r"(USGS)\t(\d*)\t([\d-]*)\t([\d.]*)", text)
+def get_usgs_sites():
+    global site_list
+    #todo create dictionary of parameter id and names
+    lake_document = requests.get(lake_list)
+    lakesoup = BeautifulSoup(lake_document.content, "lxml-xml")
+    site_no_list = [i.text for i in lakesoup.find_all("site_no")]
+    site_list = []
+    [site_list.append(i) for i in site_no_list]
+    print("{} sites indexed".format(len(site_list)))
+    return site_list
 
-    column_names =['Agency', 'site_id', 'date', 'water_level_ft']
-    temp_usgs_dataframe = pd.DataFrame(data, columns=column_names)
-    print(temp_usgs_dataframe)
+def update_usgs_database(site_list):
+    global soup
+    if isinstance(site_list, list) is True:
+        for i in site_list:
+            target_url = 'http://waterservices.usgs.gov/nwis/dv/?sites={}&siteType=LK&startDT={}&endDT={}' \
+                         '&format=waterml&variable=00062,' \
+                         '30211,62600,62614,62615,62616,62617,62618,72020,' \
+                         '72292,72293,72333,99020,72178,72199'.format(i, begin_date, end_date).replace("%2C", ",")
+            req = requests.get(target_url)
+            soup = BeautifulSoup(req.content, "lxml-xml")
+            df_rows = []
+            df = pd.DataFrame(columns=['Name', 'Date', 'Water Level [m]', 'Qualifiers'])
+            for elem in soup.find_all("value"):
+                name = elem.find("")
+                val = (elem.find("value").get_text())
+                dt = elem.find("value").attrs['dateTime']
+                qls = elem.find("value").attrs['qualifiers']
 
-    usgs_dataframe = temp_usgs_dataframe.merge(name_df)
-    usgs_dataframe.set_index(['site_name', 'site_id'], inplace=True)
-    print(len(usgs_dataframe.index.levels[0]))
-    #print(usgs_dataframe)
+    elif isinstance(site_list, str) is True:
+        target_url = 'http://waterservices.usgs.gov/nwis/dv/?sites={}&siteType=LK&startDT={}&endDT={}' \
+                     '&format=waterml&variable=00062,' \
+                     '30211,62600,62614,62615,62616,62617,62618,72020,' \
+                     '72292,72293,72333,99020,72178,72199'.format(site_list, begin_date, end_date).replace("%2C", ",")
+        print("url", target_url)
+        req = requests.get(target_url)
+        soup = BeautifulSoup(req.content, "lxml-xml")
+        df_rows = []
+        df = pd.DataFrame(columns=['Name','Date', 'Water Level [m]', 'Qualifiers'])
+        for elem in soup.find_all("value"):
+            name = elem.find("")
+            val = (elem.find("value").get_text())
+            dt = elem.find("value").attrs['dateTime']
+            qls = elem.find("value").attrs['qualifiers']
 
-    return usgs_dataframe
+    else:
+        print("site_list must be a single str or list of str")
+    # try:
+    #     return soup
+    # except Exception as e:
+
+    #     print(type(e))
+
 if __name__ == "__main__":
-    usgs_datagrab()
+    s = get_usgs_sites()[0]
+    update_usgs_database(s)
 
+
+
+
+
+
+
+
+
+    #
+    # f = open(resource_file('target_url'), 'rb').read()
+    #
+    # sites = wml(f).response
+
+    # http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+    # req = http.request('GET', target_url)
+    # text = req.data.decode()
+    #
+    # retrieved_date = re.search(r"retrieved:.*$", text, re.M)
+    # print('This data was accessed on: {}'.format(retrieved_date.group()))
+    #
+    # site_ids = re.findall(r"USGS (\d*) (.*)$", text, re.M)
+    # temp_column_names = ['site_id', 'site_name']
+    # name_df = pd.DataFrame(site_ids, columns=temp_column_names)
+    #
+    # data = re.findall(r"(USGS)\t(\d*)\t([\d-]*)\t([\d.]*)", text)
+    #
+    # column_names =['Agency', 'site_id', 'date', 'water_level_ft']
+    # temp_usgs_dataframe = pd.DataFrame(data, columns=column_names)
+    # print(temp_usgs_dataframe)
+    #
+    # usgs_dataframe = temp_usgs_dataframe.merge(name_df)
+    # usgs_dataframe.set_index(['site_name', 'site_id'], inplace=True)
+    # print(len(usgs_dataframe.index.levels[0]))
+    # #print(usgs_dataframe)
+
+    # return usgs_dataframe
+
+
+
+# if __name__ == "__main__":
+#     usgs_datagrab()
