7303ce63fd9d47586c39be2db9372f2e964222c3
jakegearon <uFZ!_g68msGpyWXoEzot>
2020-07-29 16:22:16 -0500

    rewriting USGS to intake waterML

--- db_create.py
+++ db_create.py
@@ -3,9 +3,9 @@ def update_sql(df, name):
     import pandas as pd
     from os import environ
     from sqlalchemy import create_engine
-    environ['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flask:flasktest@flask-db-identifier.cevt7olsswvw.us-east-2' \
-                                         '.rds.amazonaws.com:3306/db_name '
-
+    environ['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:lake-test1@lake-test1.cevt7olsswvw.us-east-2' \
+                                         '.rds.amazonaws.com:3306/laketest'
+    #['SQLALCHEMY_DATABASE_URI'] = 'mysql://{master username}:{db password}@{endpoint}/{db instance name}'
     db_URI = environ.get('SQLALCHEMY_DATABASE_URI')
     engine = create_engine(db_URI)
     df.to_sql(name, engine, if_exists='replace', chunksize=500, index=False)
