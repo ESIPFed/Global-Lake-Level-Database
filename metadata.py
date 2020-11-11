ad7169b5b6735a755cb1840c0e971d3c7390098e
jakegearon <uFZ!_g68msGpyWXoEzot>
2020-11-10 19:47:02 -0600

    fixed divide by 0 error on updating metadata

--- metadata.py
+++ metadata.py
@@ -3,6 +3,7 @@ def reference_table_mtadta_json(usgs_tbl):
     from sqlalchemy import create_engine
     import pymysql
     from lake_table_usgs import update_usgs_meta
+    from utiils import printProgressBar
     import json
 
     # Create database connection engines and cursor
@@ -70,25 +71,25 @@ def reference_table_mtadta_json(usgs_tbl):
 
     # Execute mysql commands, could possibly merge dicts? have not tried, see below line. 1 loop for each dict
     # merged_dict = {**hydroweb_dict, **usgs_dict, **grealm_dict}
-    from utiils import printProgressBar
-    printProgressBar(0, len(grealm_dict.items()), prefix='G-REALM:', suffix='Complete', length=50)
     sql_command = u"UPDATE `reference_ID` SET `metadata` = (%s) WHERE `id_No` = (%s);"
-    for count, key, value in enumerate(grealm_dict.items(), 1):
-        cursor.execute(sql_command, (json.dumps(value), key))
-        printProgressBar(count + 1, len(grealm_dict.items()), prefix='GREALM-USDA:', suffix='Complete', length=50)
-    connection.commit()
-
-    printProgressBar(0, len(hydroweb_dict.items()), prefix='HydroWeb:', suffix='Complete', length=50)
-    for count, key, value in enumerate(hydroweb_dict.items(), 1):
-        cursor.execute(sql_command, (json.dumps(value), key))
-        printProgressBar(count + 1, len(hydroweb_dict.items()), prefix='HydroWeb:', suffix='Complete', length=50)
-    connection.commit()
-
-    printProgressBar(0, len(usgs_dict.items()), prefix='USGS-NWIS:', suffix='Complete', length=50)
-    for count, key, value in enumerate(usgs_dict.items(), 1):
-        cursor.execute(sql_command, (json.dumps(value), key))
-        printProgressBar(count + 1, len(usgs_dict.items()), prefix='USGS-NWIS:', suffix='Complete', length=50)
-    connection.commit()
+    if len(grealm_dict.values()) > 0:
+        printProgressBar(0, len(grealm_dict.values()), prefix='G-REALM:', suffix='Complete', length=50)
+        for count, key, value in enumerate(grealm_dict.items(), 1):
+            cursor.execute(sql_command, (json.dumps(value), key))
+            printProgressBar(count + 1, len(grealm_dict.values()), prefix='GREALM-USDA:', suffix='Complete', length=50)
+        connection.commit()
+    if len(hydroweb_dict.values()) > 0:
+        printProgressBar(0, len(hydroweb_dict.values()), prefix='HydroWeb:', suffix='Complete', length=50)
+        for count, key, value in enumerate(hydroweb_dict.items(), 1):
+            cursor.execute(sql_command, (json.dumps(value), key))
+            printProgressBar(count + 1, len(hydroweb_dict.values()), prefix='HydroWeb:', suffix='Complete', length=50)
+        connection.commit()
+    if len(usgs_dict.values()) > 0:
+        printProgressBar(0, len(usgs_dict.values()), prefix='USGS-NWIS:', suffix='Complete', length=50)
+        for count, key, value in enumerate(usgs_dict.items(), 1):
+            cursor.execute(sql_command, (json.dumps(value), key))
+            printProgressBar(count + 1, len(usgs_dict.values()), prefix='USGS-NWIS:', suffix='Complete', length=50)
+        connection.commit()
     connection.close()
 
     print('Task Complete!')
